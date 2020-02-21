185529563806de08fe74ee67ede04f53e12e5af1
brandly <brandly13@gmail.com>
2020-02-21 14:24:35 -0500

    hit releases endpoint to get all major versions, back off when rate limited

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// from @tommoor
-// https://github.com/tommoor/promise-pool/issues/4
 const fs = require('fs')
 const axios = require('axios')
 const mkdirp = require('mkdirp')
@@ -15,43 +13,94 @@ const dir = './packages'
   const pool = new PromisePool({ concurrency: 5 })
 
   for (let i = 0; i < data.length; i++) {
-    const { name, version, versions } = data[i]
-    // `search.json` response seems to have changed in the past week
-    if (version) {
-      pool.add(() => fetchnwrite(name, version))
-    } else if (versions instanceof Array) {
-      for (let j = 0; j < versions.length; j++) {
-        pool.add(() => fetchnwrite(name, versions[j]))
-      }
-    }
+    const { name } = data[i]
+    const versions = await getVersions(name)
+    const majorVersions = uniqBy(versions, v => v.split('.')[0])
+
+    majorVersions.forEach(version => {
+      pool.add(() => downloadPackage(name, version))
+    })
   }
 
   await pool.all()
 })()
 
-const fetchnwrite = async (name, version) => {
+const getVersions = async name => {
+  const url = `https://package.elm-lang.org/packages/${name}/releases.json`
+  const res = await axios.get(url)
+  return Object.keys(res.data)
+}
+
+const downloadPackage = async (name, version) => {
   try {
-    const elmJson = await getElmJson(name, version)
-    write(name, version, elmJson)
+    const elmJson = await getFileAtVersion(name, 'elm.json', version)
+    writeFile(name, version, elmJson)
   } catch (e) {
-    if (e.response && e.response.status !== 404) {
-      console.log(`Error with ${name}@${version}`)
-      console.log(
-        `https://api.github.com/repos/${name}/contents/elm.json?ref=${version}`
-      )
+    if (e.response && e.response.status === 404) {
+      return await downloadOldPackage(name, version)
+    } else {
+      console.log(`Error with elm.json ${name}@${version}`)
       console.log(e)
     }
   }
 }
 
-const write = (name, v, json) => {
-  mkdirp.sync(`${dir}/${name.split('/')[0]}`)
-  fs.writeFileSync(`${dir}/${name}@${v}.json`, json)
+const downloadOldPackage = async (name, version) => {
+  try {
+    const elmJson = await getFileAtVersion(name, 'elm-package.json', version)
+    // old packages didn't include their own name...
+    const pkg = { ...JSON.parse(elmJson), name }
+    writeFile(name, version, JSON.stringify(pkg, null, 2))
+  } catch (e) {
+    if (e.response && e.response.status === 404) {
+      console.log('404', name, version)
+    } else {
+      console.log(`Error with elm-package.json ${name}@${version}`)
+      console.log(e)
+    }
+  }
 }
 
-const getElmJson = async (name, v) => {
-  const url = `https://api.github.com/repos/${name}/contents/elm.json?ref=${v}&access_token=${token}`
-  const elmJson = await axios.get(url)
-  const buf = Buffer.from(elmJson.data.content, 'base64')
-  return buf.toString('utf-8')
+const writeFile = (name, version, elmJson) => {
+  const [username, pkgname] = name.split('/')
+  mkdirp.sync(`${dir}/${username}`)
+  fs.writeFileSync(`${dir}/${username}/${pkgname}@${version}.json`, elmJson)
 }
+
+const getFileAtVersion = async (repo, filename, version) => {
+  const url = `https://api.github.com/repos/${repo}/contents/${filename}?ref=${version}&access_token=${token}`
+  const res = await respectfulGET(url)
+  return Buffer.from(res.data.content, 'base64').toString('utf-8')
+}
+
+const uniqBy = (list, by) => {
+  let obj = {}
+  for (let i = 0; i < list.length; i++) {
+    let item = list[i]
+    let key = by(item)
+    obj[key] = item
+  }
+  return Object.values(obj)
+}
+
+const respectfulGET = async url => {
+  try {
+    return await axios.get(url)
+  } catch (e) {
+    if (
+      e.response &&
+      e.response.status === 403 &&
+      (e.response.data.message.includes('abuse') ||
+        e.response.data.message.includes('rate limit'))
+    ) {
+      // rate limit!
+      console.log('sleep!')
+      await sleep(30 * 1000)
+      return await respectfulGET(url)
+    } else {
+      throw e
+    }
+  }
+}
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
