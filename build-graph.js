766b5429d67d2cc753a3a69e9fcfee0a0325a52c
brandly <brandly13@gmail.com>
2020-02-24 00:05:06 -0500

    index using published name (not pkg.name), better error output, better version comparison

--- build-graph.js
+++ build-graph.js
@@ -28,17 +28,27 @@ const readJson = p => {
 
   const packageLookup = {}
   const insert = (path, pkg) => {
-    if (`./packages/${pkg.name}@${pkg.version}.json` !== path) {
-      console.log('package doesnt match path:', path)
-      console.log(pkg)
-      return
+    let [name, version] = parseFilePath(path)
+    if (pkg.name !== name) {
+      console.log(
+        `package name doesn't match: Published as ${name}@${version}, elm.json says ${
+          pkg.name
+        }`
+      )
     }
-    if (!(pkg.name in packageLookup)) {
-      packageLookup[pkg.name] = {}
+    if (pkg.version !== version) {
+      console.log(
+        `package version doesn't match: Published as ${name}@${version}, elm.json says ${
+          pkg.version
+        }`
+      )
+    }
+    if (!(name in packageLookup)) {
+      packageLookup[name] = {}
     }
 
-    packageLookup[pkg.name][pkg.version] = pkg
-    graph.addNode(pkgName(pkg), pkg)
+    packageLookup[name][version] = pkg
+    graph.addNode(pkgName({ name, version }), pkg)
   }
 
   for (let i = 0; i < filePaths.length; i++) {
@@ -60,13 +70,15 @@ const readJson = p => {
     for (let dep in deps) {
       const isMatchingVersion = parseRange(deps[dep])
       if (!(dep in packageLookup)) {
-        console.log('Unknown dep:', dep, deps[dep], '\nfor:', name, version)
+        console.log('Unknown dep:', dep, deps[dep])
+        console.log('needed for:', name, version)
         continue
       }
       const depVersion = Object.keys(packageLookup[dep]).find(isMatchingVersion)
       if (!depVersion) {
         console.log(`${name}@${version}`)
         console.log('no matching version:', dep, deps[dep])
+        console.log('available:', Object.keys(packageLookup[dep]).join(', '))
         continue
       }
       graph.addLink(
@@ -106,15 +118,49 @@ const pkgName = pkg => `${pkg.name}@${pkg.version}`
 
 const parseFilePath = path =>
   path
-    .replace(/^\.\/packages\//, '')
+    .replace(/^\.\/all-packages\//, '')
     .replace(/\.json$/, '')
     .split('@')
 
 const parseRange = range => {
   // 1.0.0 <= v < 2.0.0
-  const splits = range.split('<'),
-    low = splits[0].trim(),
-    high = splits[2].trim()
+  const splits = range
+      .replace(/\<\=/g, '')
+      .replace(/\</g, '')
+      .split(' ')
+      .filter(Boolean),
+    low = parseVersion(splits[0].trim()),
+    high = parseVersion(splits[2].trim())
 
-  return version => version >= low && version < high
+  return version => {
+    let v = parseVersion(version)
+    // version >= low && version < high
+    // console.log({ low, high }, v)
+    return gte(v, low) && lt(v, high)
+  }
+}
+
+const parseVersion = str =>
+  str
+    .split('.')
+    .map(v => parseInt(v))
+    .slice(0, 3)
+
+const gte = (a, b) => {
+  let comp = compare(a, b)
+  return comp === 'gt' || comp === 'eq'
+}
+const lt = (a, b) => compare(a, b) === 'lt'
+const compare = (a, b) => {
+  if (!a.length || !b.length) {
+    return 'eq'
+  }
+
+  if (a[0] === b[0]) {
+    return compare(a.slice(1), b.slice(1))
+  } else if (a[0] < b[0]) {
+    return 'lt'
+  } else {
+    return 'gt'
+  }
 }
