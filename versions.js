d62292f5ccb0f67ba2c8d56831addecdb05eb635
brandly <brandly13@gmail.com>
2020-02-24 13:34:44 -0500

    actually parse version ranges

--- versions.js
+++ versions.js
@@ -2,19 +2,15 @@ const assert = require('assert')
 
 const parseRange = range => {
   // 1.0.0 <= v < 2.0.0
-  const splits = range
-      .replace(/\<\=/g, '')
-      .replace(/\</g, '')
-      .split(' ')
-      .filter(Boolean),
+  const splits = range.split(' '),
     low = parseVersion(splits[0].trim()),
-    high = parseVersion(splits[2].trim())
+    high = parseVersion(splits[4].trim()),
+    first = getComp(splits[1]),
+    second = getComp(splits[3])
 
   return version => {
     let v = parseVersion(version)
-    // version >= low && version < high
-    // console.log({ low, high }, v)
-    return gte(v, low) && lt(v, high)
+    return first(low, v) && second(v, high)
   }
 }
 
@@ -24,11 +20,30 @@ const parseVersion = str =>
     .map(v => parseInt(v))
     .slice(0, 3)
 
+const gt = (a, b) => compare(a, b) === 'gt'
 const gte = (a, b) => {
   let comp = compare(a, b)
   return comp === 'gt' || comp === 'eq'
 }
 const lt = (a, b) => compare(a, b) === 'lt'
+const lte = (a, b) => {
+  let comp = compare(a, b)
+  return comp === 'lt' || comp === 'eq'
+}
+const getComp = str => {
+  switch (str) {
+    case '>':
+      return gt
+    case '>=':
+      return gte
+    case '<':
+      return lt
+    case '<=':
+      return lte
+    default:
+      throw new Error(`Unexpected getComp str: ${str}`)
+  }
+}
 const compare = (a, b) => {
   if (typeof a === 'string') {
     a = parseVersion(a)
@@ -50,6 +65,7 @@ const compare = (a, b) => {
 }
 
 assert.equal(compare('1.0.10', '1.0.9'), 'gt')
+assert.equal(parseRange('2.0.0 <= v <= 2.1.0')('2.1.0'), true)
 
 module.exports = {
   parseRange,
